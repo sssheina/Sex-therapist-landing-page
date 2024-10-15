2cf4e0f0d9e8b330102c7a0df0a86dad3b1aac80
Olga Bulgakova <108359930+OlgaBuLL@users.noreply.github.com>
2024-10-15 21:39:20 +0200

    switch languages

--- assets/scripts/composables/language.js
+++ assets/scripts/composables/language.js
@@ -28,3 +28,24 @@ export const loadLanguage = (generateContent) => {
     generateContent(selectedLanguage);
   });
 };
+
+const language = document.querySelector(".language__language");
+const languageOptionsList = document.querySelector(".language__options");
+const languageOptions = document.querySelectorAll(".language__option");
+
+language.addEventListener("click", () => {
+  languageOptionsList.classList.toggle("active");
+});
+
+languageOptions.forEach((option) => {
+  option.addEventListener("click", () => {
+    if (option.textContent === "RU") {
+      window.location.href = "ru.html";
+    } else if (option.textContent === "FR") {
+      window.location.href = "fr.html";
+    } else window.location.href = "index.html";
+
+    language.textContent = option.textContent;
+    languageOptionsList.classList.remove("active");
+  });
+});
